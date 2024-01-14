94a8b88a114b0c098a1570a993b1fa2e414297a9
Eason Chen <arx827@gmail.com>
2024-01-14 17:47:29 +0800

    [feature] 『init』

---
+++ docs/.vitepress/configs/navbar.ts
@@ -0,0 +1,15 @@
+export const nav = [
+  {
+    text: 'Test',
+    items: [
+      {
+        text: 'Test 子層',
+        link: '/page/guide/test.md'
+      },
+    ]
+  },
+  {
+    text: '標題2',
+    link: '標題2 相對路徑'
+  },
+]
